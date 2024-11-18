54e425a5a7f27f5f0b41bb30b66cff39921b924e
Diana Arazklycheva <arazklycheva94@mail.ru>
2024-11-18 13:28:08 +0500

    build на gh-pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,10 +11,10 @@ class Card extends React.Component {
             <div className={'card ' + (this.props.isShowed ? 'opened' : 'closed')}
                 onClick={this.cardClickHandler.bind(this, this.props.item)}>
                 <div className="card-inner card-front">
-                    <img src={'/images/' + this.props.item.image} alt={this.props.item.name} />
+                    <img src={'images/' + this.props.item.image} alt={this.props.item.name} />
                 </div>
                 <div className="card-inner card-back">
-                    <img src={'/images/question.svg'} alt='question mark' />
+                    <img src={'images/question.svg'} alt='question mark' />
                 </div>
             </div>
         )
